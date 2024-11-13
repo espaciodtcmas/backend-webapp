34483f5e868b8e27313f28851031c285b72b6897
Luis Zamorano <contacto@luiszamorano.cl>
2024-11-13 06:24:27 -0300

    fix(index): anadiendo cors

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ import regionRoutes from './routes/region.route.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
-app.use(cors());
+const corsOptions = {
+  origin: 'http://frontend-webapp:80',
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use('/region', regionRoutes);
 
